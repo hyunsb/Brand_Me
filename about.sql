154f0d0025c5effc44e98bbd35b393524fbd4383
hyunb laptop <jhss0113@naver.com>
2022-06-03 12:08:37 +0900

    update

--- about.sql
+++ about.sql
@@ -1,10 +1,11 @@
 create table about (
-	user_num int auto_increment,
-	user_id varchar(10),
-	about_img varchar(50),
-	about_introduction varchar(20),
-	experience int,
-	project int,
-	worked varchar(20),
-	primary key(user_num)
+  `user_num` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(10) DEFAULT NULL,
+  `about_img` varchar(50) DEFAULT NULL,
+  `about_information` varchar(20) DEFAULT NULL,
+  `experience` int(11) DEFAULT NULL,
+  `project` int(11) DEFAULT NULL,
+  `worked` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`user_num`)
+
 );
